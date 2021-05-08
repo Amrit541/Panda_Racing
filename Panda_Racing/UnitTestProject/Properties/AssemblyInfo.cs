746f999451774a2195efebdb6be15619ab427ab9
Amritpreet Singh <amritrandhawa31@yahoo.com>
2021-05-08 14:11:39 +1200

    Add project files.

---
+++ Panda_Racing/UnitTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("UnitTestProject")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bc3da572-0169-4bf5-b89c-d57c1c2c67fb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Panda_Racing/UnitTestProject/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Panda_Racing;
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        Bet Obj_Bet;
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Obj_Bet.PandaNum = 4;
+            Obj_Bet.Amount = 40;
+            Assert.AreEqual(40, Obj_Bet.Pay(4));
+        }
+    }
+}
